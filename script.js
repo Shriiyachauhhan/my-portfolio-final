7d2cfd0c1b87b2b4e06c3aa4d5bd42cf9ff4e8d9
Shriiyachauhhan <shriyachauhan521@gmail.com>
2025-08-18 01:18:30 +0530

    changes made

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const burger = document.getElementById('burger');
-const navbar = document.querySelector('.navbar');
 
-burger.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
